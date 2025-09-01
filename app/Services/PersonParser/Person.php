8e65addc3a87b9604705940aa839723379e751d0
thabo-moyo <thabo.s.moyo@gmail.com>
2025-09-01 15:48:52 +0100

    person dto

---
+++ app/Services/PersonParser/Person.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\Services\PersonParser;
+
+use app\Services\PersonParser\Fields\FirstName;
+use app\Services\PersonParser\Fields\Initial;
+use app\Services\PersonParser\Fields\LastName;
+use app\Services\PersonParser\Fields\Title;
+
+readonly class Person
+{
+    public function __construct(
+        private Title $title,
+        private ?FirstName $firstName,
+        private ?Initial $initial,
+        private LastName $lastName
+    ) {
+    }
+
+    public function getTitle(): Title
+    {
+        return $this->title;
+    }
+
+    public function getFirstName(): FirstName
+    {
+        return $this->firstName;
+    }
+
+    public function getInitial(): Initial
+    {
+        return $this->initial;
+    }
+
+    public function getLastName(): LastName
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * We will ise this to make sure required fields are present
+     * @return string[]
+     */
+    public static function requiredFields(): array
+    {
+        return [
+            Title::class,
+            LastName::class
+        ];
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'title' => $this->title->getValue(),
+            'first_name' => $this->firstName?->getValue() ?? '',
+            'initial' => $this->initial?->getValue() ?? '',
+            'last_name' => $this->lastName->getValue(),
+        ];
+    }
+
+}
+
