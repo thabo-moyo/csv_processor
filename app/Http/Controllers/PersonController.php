149bd80dbae6c42a2673734d43424ebe5cf990c9
thabo-moyo <thabo.s.moyo@gmail.com>
2025-09-01 15:10:49 +0100

    generic csv upload handler

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller
@@ -13,4 +14,17 @@ class PersonController extends Controller
             $persons = Person::paginate(100);
             return response()->json($persons);
         }
+    public function processCsv(Request $request): JsonResponse
+    {
+        $request->validate([
+            'file' => 'required|file|mimes:csv'
+        ]);
+
+        $file = $request->file('file');
+
+        return response()->json([
+        'data' => [
+        ]
+        ]);
+    }
 }
